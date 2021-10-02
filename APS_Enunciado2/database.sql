a062caec1d9d79166c8ec5432530150c88643015
Goinza <gonzalo-am@hotmail.com>
2021-10-02 20:07:52 -0300

    Cambios en las tablas de vacunas aplicadas y usuarios
    Se dividio la tabla Vacunas_Aplicadas: se convirtio en una relacion entre las tablas Vacunas, Personas y Regiones_Sanitarias. Esta ultima tiene una relacion con la tabla Provincias.
    Se unio la tabla Adminstradores a la tabla Usuarios

--- APS_Enunciado2/database.sql
+++ APS_Enunciado2/database.sql
@@ -2,21 +2,68 @@ CREATE DATABASE vacunas;
 
 USE vacunas;
 
-CREATE TABLE Vacunas_Aplicadas (
+CREATE TABLE Personas (
     nombre VARCHAR(25) NOT NULL,
     apellido VARCHAR(25) NOT NULL,
     fecha_nacimiento DATE NOT NULL,
-    vacuna VARCHAR(25) NOT NULL,
-    primera_dosis DATE NOT NULL,
-    segunda_dosis DATE,
-    cantidad_dosis INT UNSIGNED NOT NULL,
     mail VARCHAR(50) NOT NULL,
     dni INT UNSIGNED NOT NULL,
-    provincia VARCHAR(25) NOT NULL,
-    region_sanitaria INT UNSIGNED NOT NULL,
 
     CONSTRAINT pk_dni
     PRIMARY KEY (dni)
+) ENGINE=InnoDB;
+
+CREATE TABLE Vacunas (
+    nombre VARCHAR(25) NOT NULL,
+    id INT AUTO_INCREMENT,
+
+    CONSTRAINT pk_vacuna
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE Provincias (
+    nombre VARCHAR(25) NOT NULL,
+    id INT AUTO_INCREMENT,
+
+    CONSTRAINT pk_provincia
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE Regiones_Sanitarias (
+    id_provincia INT NOT NULL,
+    id_region INT NOT NULL,
+
+    CONSTRAINT pk_region
+    PRIMARY KEY (id_provincia, id_region),
+
+    CONSTRAINT fk_provincia
+    FOREIGN KEY (id_provincia) REFERENCES Provincias (id)
+     ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB;
+
+CREATE TABLE Vacunas_Aplicadas (
+    primera_dosis DATE NOT NULL,
+    segunda_dosis DATE,
+    cantidad_dosis INT UNSIGNED NOT NULL,
+    dni INT UNSIGNED NOT NULL,
+    id_vacuna INT,
+    provincia INT NOT NULL,
+    id_region INT NOT NULL,
+
+    CONSTRAINT pk_aplicadas
+    PRIMARY KEY (dni),
+
+    CONSTRAINT fk_aplicada_persona
+    FOREIGN KEY (dni) REFERENCES Personas (dni)
+     ON DELETE RESTRICT ON UPDATE RESTRICT,
+
+    CONSTRAINT fk_aplicada_vacuna
+    FOREIGN KEY (id_vacuna) REFERENCES Vacunas (id)
+     ON DELETE RESTRICT ON UPDATE RESTRICT,
+
+    CONSTRAINT fk_aplicada_region
+    FOREIGN KEY (provincia, id_region) REFERENCES Regiones_Sanitarias (id_provincia, id_region)
+     ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB;
 
 CREATE TABLE Vacunas_Entregadas (
@@ -26,20 +73,7 @@ CREATE TABLE Vacunas_Entregadas (
 
     CONSTRAINT pk_entrega
     PRIMARY KEY (provincia, vacuna)
-) ENGINE=InnoDB
-
-CREATE TABLE Administradores (
-    usuario VARCHAR(25) NOT NULL,
-    contraseña CHAR(32) NOT NULL,
-    nombre VARCHAR(25) NOT NULL,
-    apellido VARCHAR(25) NOT NULL,
-    mail VARCHAR(50) NOT NULL,
-    dni INT UNSIGNED NOT NULL,
-    telefono INT UNSIGNED NOT NULL,
-
-    CONSTRAINT pk_admin
-    PRIMRAY KEY (usuario)
-) ENGINE=InnoDB
+) ENGINE=InnoDB;
 
 CREATE TABLE Usuarios (
     usuario VARCHAR(25) NOT NULL,
@@ -51,10 +85,11 @@ CREATE TABLE Usuarios (
     dni INT UNSIGNED NOT NULL,
     telefono INT UNSIGNED NOT NULL,
     domicilio VARCHAR(50) NOT NULL,
+    es_admin BOOLEAN NOT NULL,
 
     CONSTRAINT pk_usuario
-    PRIMARY KEY usuario    
-)
+    PRIMARY KEY (usuario)
+) ENGINE=InnoDB;
 
 CREATE USER "admin"@"localhost" IDENTIFIED BY 'admin';
 GRANT ALL PRIVILEGES ON vacunas.* TO "admin"@"localhost" WITH GRANT OPTION;
