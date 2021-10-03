3896fe336b950f0d39e7b03defe61785c8d2779d
Goinza <gonzalo-am@hotmail.com>
2021-10-03 18:40:22 -0300

    Se agrego una interfaz para acceder a las tablas de la base de datos
    Tambien se agrego una implementacion mockup como ejemplo para utilizarla
    en tests

---
+++ APS_Enunciado2/src/Model/ModeloTabla.java
@@ -0,0 +1,20 @@
+package Model;
+
+import java.util.Vector;
+
+/**
+ * Interfaz para acceder a una tabla de la base de datos.
+ * Cada implementacion de esta interfaz accedera a una tabla (o vista) diferente.
+ */
+
+public interface ModeloTabla {
+
+	public Vector<String> obtenerNombresColumnas();
+	
+	public Vector<Vector<Object>> obtenerTuplas();
+	
+	public boolean agregarTupla(Vector<Object> tupla);
+	
+	public boolean modificarTupla(Vector<Object> tupla);
+	
+}
---
+++ APS_Enunciado2/src/Model/TestTabla.java
@@ -0,0 +1,50 @@
+package Model;
+
+import java.util.Vector;
+
+/**
+ * Implementación mockup - siempre retorna los mismos valores.
+ *
+ */
+public class TestTabla implements ModeloTabla {
+
+	@Override
+	public Vector<String> obtenerNombresColumnas() {
+		Vector<String> columna = new Vector<String>();
+		columna.add("Nombre");
+		columna.add("Apellido");
+		columna.add("DNI");
+		return columna;
+	}
+
+	@Override
+	public Vector<Vector<Object>> obtenerTuplas() {
+		Vector<Vector<Object>> tuplas = new Vector<Vector<Object>>();
+		Vector<Object> t;
+		
+		t = new Vector<Object>();
+		t.add("Roberto");
+		t.add("Perez");
+		t.add(32122758);
+		tuplas.add(t);
+		
+		t = new Vector<Object>();
+		t.add("Luis");
+		t.add("Miguel");
+		t.add(26412517);
+		tuplas.add(t);
+				
+		return tuplas;
+	}
+
+	@Override
+	public boolean agregarTupla(Vector<Object> tupla) {
+		return true;
+	}
+
+	@Override
+	public boolean modificarTupla(Vector<Object> tupla) {
+		return true;
+	}
+
+}
