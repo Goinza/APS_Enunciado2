533cd47a2c4de597487f2cca249e00035f6db5fe
Goinza <gonzalo-am@hotmail.com>
2021-10-14 21:02:47 -0300

    Update ModeloNuevoUsuario.java
    Existe un problema con agregarUsuario, ya que durante la explicacion del US de alta de usuario se mostraban menos elementos que en la explicacion de las tablas relaciones.
    Es muy probable que se tenga que eliminar atributos de la tabla Usuarios en SQL o que se neceistan mas atributos en la llamada del metodo agregarUsuario

--- APS_Enunciado2/src/Model/ModeloNuevoUsuario.java
+++ APS_Enunciado2/src/Model/ModeloNuevoUsuario.java
@@ -1,14 +1,36 @@
 package Model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import resources.DBConnection;
+
 public class ModeloNuevoUsuario {
 	
 	public boolean existeUsuario(String nombre) {
-		
-		return false;
+		DBConnection database = DBConnection.getInstance();
+		ResultSet result = database.realizarConsulta("select * from Usuarios where usuario='"+nombre+"';");
+		boolean existe = false;
+		try {
+			existe = result.next();
+		}
+		catch (SQLException e) {
+			
+		}
+		return existe;
 	}
 
 	public void agregarUsuario(String usuario, String password, String email, String telefono, String cargo) {
-		// TODO Auto-generated method stub
+		DBConnection database = DBConnection.getInstance();
+		String nombre = "Juan";
+		String apellido = "Perez";
+		String mail = "hola@hotmail.com";
+		int dni = 10;
+		String domicilio = "Alem 203";
+		String query = "insert into Usuarios values ('" + usuario + "', '" + password + "', '" + nombre + "', '"
+				+ apellido + "', '" + cargo + "', '" + mail + ", " + dni + ", " + telefono + ", '" + domicilio
+				+ "', " + false;
+		database.realizarStatement(query);
 		
 	}
 	
